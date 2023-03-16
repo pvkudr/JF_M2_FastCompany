76857e72276668add40a5719cee0e75198c566be
K P <pvkudr@gmail.com>
2023-03-16 23:21:24 +0800

    task2 m2_3

---
+++ src/components/qualitie.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Quality = function (qualities) {
+    return qualities.map ( (item) => (
+        <span
+            className={'m-2 badge bg-' + item.color}
+            key={item._id}
+        >
+                {item.name}
+            </span>)
+    )
+}
+
+export default Quality
